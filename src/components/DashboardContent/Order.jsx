f4de3deae3d886382e20bc64f1f41737a47fa595
Ayush Suryan <suryanayush@gmail.com>
2024-10-20 23:50:59 +0530

    update

--- src/components/DashboardContent/Order.jsx
+++ src/components/DashboardContent/Order.jsx
@@ -1,50 +1,140 @@
 import React from "react";
-import { Card, CardContent, Typography } from "@mui/material";
+import { CardContent, Typography } from "@mui/material";
 import "./Order.css"; // Custom CSS for glassy card styles
 
 const Order = () => {
   return (
     <div className="order-container">
       <h2>Orders Page</h2>
-      <div className="glassy-card">
-        <CardContent>
-          <Typography variant="h5" component="div" className="card-title">
-            Your Orders
-          </Typography>
-          <Typography variant="body2" className="card-content">
-            Here you can view and manage all your orders.
-          </Typography>
-        </CardContent>
-      </div>
-      <div className="glassy-card">
-        <CardContent>
-          <Typography variant="h5" component="div" className="card-title">
-            Your Orders
-          </Typography>
-          <Typography variant="body2" className="card-content">
-            Here you can view and manage all your orders.
-          </Typography>
-        </CardContent>
-      </div>
-      <div className="glassy-card">
-        <CardContent>
-          <Typography variant="h5" component="div" className="card-title">
-            Your Orders
-          </Typography>
-          <Typography variant="body2" className="card-content">
-            Here you can view and manage all your orders.
-          </Typography>
-        </CardContent>
-      </div>
-      <div className="glassy-card">
-        <CardContent>
-          <Typography variant="h5" component="div" className="card-title">
-            Your Orders
-          </Typography>
-          <Typography variant="body2" className="card-content">
-            Here you can view and manage all your orders.
-          </Typography>
-        </CardContent>
+      <div className="scrollable-container">
+        <div className="glassy-card">
+          <CardContent>
+            <img src="../../src/assets/rig1.svg" alt="rig1" width="50%" />
+            <Typography variant="h5" component="div" className="card-title">
+              RIG UNIXO BRONZE
+            </Typography>
+            <Typography variant="body2" className="card-content">
+              <div>Cost: 4000 INR</div>
+              <div>Daily Return: 80 Rs</div>
+              <div>Duration: 90 days</div>
+            </Typography>
+            <button className="login-button" role="button">
+              Buy Now
+            </button>
+          </CardContent>
+        </div>
+        <div className="glassy-card">
+          <CardContent>
+            <img src="../../src/assets/rig1.svg" alt="rig1" width="50%" />
+            <Typography variant="h5" component="div" className="card-title">
+              RIG UNIXO BRONZE
+            </Typography>
+            <Typography variant="body2" className="card-content">
+              <div>Cost: 4000 INR</div>
+              <div>Daily Return: 80 Rs</div>
+              <div>Duration: 90 days</div>
+            </Typography>
+            <button className="login-button" role="button">
+              Buy Now
+            </button>
+          </CardContent>
+        </div>
+        <div className="glassy-card">
+          <CardContent>
+            <img src="../../src/assets/rig1.svg" alt="rig1" width="50%" />
+            <Typography variant="h5" component="div" className="card-title">
+              RIG UNIXO BRONZE
+            </Typography>
+            <Typography variant="body2" className="card-content">
+              <div>Cost: 4000 INR</div>
+              <div>Daily Return: 80 Rs</div>
+              <div>Duration: 90 days</div>
+            </Typography>
+            <button className="login-button" role="button">
+              Buy Now
+            </button>
+          </CardContent>
+        </div>
+        <div className="glassy-card">
+          <CardContent>
+            <img src="../../src/assets/rig1.svg" alt="rig1" width="50%" />
+            <Typography variant="h5" component="div" className="card-title">
+              RIG UNIXO BRONZE
+            </Typography>
+            <Typography variant="body2" className="card-content">
+              <div>Cost: 4000 INR</div>
+              <div>Daily Return: 80 Rs</div>
+              <div>Duration: 90 days</div>
+            </Typography>
+            <button className="login-button" role="button">
+              Buy Now
+            </button>
+          </CardContent>
+        </div>
+        <div className="glassy-card">
+          <CardContent>
+            <img src="../../src/assets/rig1.svg" alt="rig1" width="50%" />
+            <Typography variant="h5" component="div" className="card-title">
+              RIG UNIXO BRONZE
+            </Typography>
+            <Typography variant="body2" className="card-content">
+              <div>Cost: 4000 INR</div>
+              <div>Daily Return: 80 Rs</div>
+              <div>Duration: 90 days</div>
+            </Typography>
+            <button className="login-button" role="button">
+              Buy Now
+            </button>
+          </CardContent>
+        </div>
+        <div className="glassy-card">
+          <CardContent>
+            <img src="../../src/assets/rig1.svg" alt="rig1" width="50%" />
+            <Typography variant="h5" component="div" className="card-title">
+              RIG UNIXO BRONZE
+            </Typography>
+            <Typography variant="body2" className="card-content">
+              <div>Cost: 4000 INR</div>
+              <div>Daily Return: 80 Rs</div>
+              <div>Duration: 90 days</div>
+            </Typography>
+            <button className="login-button" role="button">
+              Buy Now
+            </button>
+          </CardContent>
+        </div>
+        <div className="glassy-card">
+          <CardContent>
+            <img src="../../src/assets/rig1.svg" alt="rig1" width="50%" />
+            <Typography variant="h5" component="div" className="card-title">
+              RIG UNIXO BRONZE
+            </Typography>
+            <Typography variant="body2" className="card-content">
+              <div>Cost: 4000 INR</div>
+              <div>Daily Return: 80 Rs</div>
+              <div>Duration: 90 days</div>
+            </Typography>
+            <button className="login-button" role="button">
+              Buy Now
+            </button>
+          </CardContent>
+        </div>
+        <div className="glassy-card">
+          <CardContent>
+            <img src="../../src/assets/rig1.svg" alt="rig1" width="50%" />
+            <Typography variant="h5" component="div" className="card-title">
+              RIG UNIXO BRONZE
+            </Typography>
+            <Typography variant="body2" className="card-content">
+              <div>Cost: 4000 INR</div>
+              <div>Daily Return: 80 Rs</div>
+              <div>Duration: 90 days</div>
+            </Typography>
+            <button className="login-button" role="button">
+              Buy Now
+            </button>
+          </CardContent>
+        </div>
       </div>
     </div>
   );
