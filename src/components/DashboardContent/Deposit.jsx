45d2f66f1f4aeaa7675b110babba3305f0425583
Ayush Suryan <suryanayush@gmail.com>
2024-10-21 05:46:17 +0530

    Wallet Design

--- src/components/DashboardContent/Deposit.jsx
+++ src/components/DashboardContent/Deposit.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { CardContent, Typography } from "@mui/material";
+import "./Deposit.css"; // Custom CSS for glassy card styles
 
 const Deposit = () => {
+  // Function to simulate mining start
+
   return (
     <div>
-      <h2>Deposit Page</h2>
+      <div className="order-container">
+        <h2>Wallet</h2>
+        <div className="scrollable-container">
+          <div className="glassy-card">
+            <CardContent>
+              <Typography variant="h5" component="div" className="card-title">
+                Balance: 5000 INR
+              </Typography>
+
+              <button className="login-button" role="button">
+                Deposit
+              </button>
+              <button className="login-button" role="button">
+                Withdraw
+              </button>
+            </CardContent>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
